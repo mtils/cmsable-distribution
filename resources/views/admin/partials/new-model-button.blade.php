e5776247cebde64bf513bdc79e262f4e7fc4e28b
Michael Tils <mtils@web-utils.de>
2015-05-29 14:36:23 +0200

    Small layout fixes

--- resources/views/admin/partials/new-model-button.blade.php
+++ resources/views/admin/partials/new-model-button.blade.php
@@ -1,9 +1,16 @@
     @if(isset($allowCreate) && $allowCreate)
-    <? $classTitle = Lang::choice($modelLangKey,1);
+    <? 
+        if (isset($modelLangKey)) {
+            $classTitle = Lang::choice($modelLangKey,1);
+            $buttonText = Lang::get('ems::action.create-model', ['model'=>$classTitle]);
+        }
+        elseif (isset($createButtonText)) {
+            $buttonText = $createButtonText;
+        }
         $nextRouteName = str_replace('index','create', Route::currentRouteName());
     ?>
-    <div class="btn btn-success btn-file" style="width: {{ (strlen($classTitle) < 10) ? '135' : strlen($classTitle)*10 }}px;" onclick="window.location='{{ URL::route($nextRouteName) }}'">
+    <div class="btn btn-success btn-file" style="width: {{ (strlen($buttonText) < 10) ? '135' : strlen($buttonText)*10 }}px;" onclick="window.location='{{ URL::route($nextRouteName) }}'">
         <i class="fa fa-edit"></i>
-        Neue {{ $classTitle }}
+        {{ $buttonText }}
     </div><br/><br/>
     @endif
