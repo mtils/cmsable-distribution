4c4979cfd709676b166461b8df390dbbc01cf925
Michael Tils <mtils@web-utils.de>
2015-06-09 16:04:09 +0200

    Added phpunit structure and corrected autoload entry

---
+++ tests/unit/Validators/PasswordEmailValidatorTest.php
@@ -0,0 +1,27 @@
+<?php 
+
+use Mockery as m;
+use Ems\App\Validators\PasswordEmailValidator;
+
+class PasswordEmailValidatorTest extends PHPUnit_Framework_TestCase
+{
+
+    public function test()
+    {
+        $this->assertInstanceOf(
+            'Cmsable\Resource\Contracts\Validator',
+            $this->newValidator()
+        );
+    }
+
+    protected function newValidator()
+    {
+        return new PasswordEmailValidator;
+    }
+
+    public function tearDown()
+    {
+        m::close();
+    }
+
+}
