b8d92a5f586588822590d37865e31c736cf9116e
Michael Tils <mtils@web-utils.de>
2016-09-05 11:31:21 +0200

    Added id to div

--- resources/views/admin/partials/bootstrap-modal.blade.php
+++ resources/views/admin/partials/bootstrap-modal.blade.php
@@ -1,6 +1,6 @@
 <div class="modal fade" id="inline-modal">
   <div class="modal-dialog">
-    <div class="modal-content">
+    <div class="modal-content" id="modal-ajax-content">
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
         <h4 class="modal-title"></h4>
