d4d89643674d326dd59936cb66cb181cf275c614
Michael Tils <mtils@web-utils.de>
2016-10-04 13:16:20 +0200

    Added month names

--- resources/lang/de/base.php
+++ resources/lang/de/base.php
@@ -7,6 +7,20 @@ return [
     'date-format'         => 'd.m.Y',
     'time-format'         => 'H:i',
     'datetime-format'     => 'd.m.Y H:i',
-    'create-object'       => ':class anlegen'
+    'create-object'       => ':class anlegen',
+    'months'=> [
+        '1'     =>  'Januar',
+        '2'     =>  'Februar',
+        '3'     =>  'März',
+        '4'     =>  'April',
+        '5'     =>  'Mai',
+        '6'     =>  'Juni',
+        '7'     =>  'Juli',
+        '8'     =>  'August',
+        '9'     =>  'September',
+        '10'    =>  'Oktober',
+        '11'    =>  'November',
+        '12'    =>  'Dezember'
+    ]
 
 ];
